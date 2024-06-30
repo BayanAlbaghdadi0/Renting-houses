32795b52f120b51d8270e566f96814e48bcdfd62
Ahmad <ahmadzer607@gmail.com>
2024-06-30 13:46:57 +0300

    f

--- backend/controller/loginController.js
+++ backend/controller/loginController.js
@@ -1,33 +1,40 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Client = require("../model/clientModel");
-// const generateTokenAndSetCookie = require("../utils/generateToken");
+require("dotenv").config();
 
-function test() {
+function test(req,res) {
   console.log("test");
-}async function login(req, res) {
+  console.log(process.env.MY_SECRET);
+}
+async function login(req, res) {
   const { username, password } = req.body;
-  console.log(password);
-  const user = await Client.findOne({ username });
+console.log(req.body);
+const user = await Client.findOne({ username });
 
-  if (!user) {
-    return res.status(404).json({ message: 'User not found' });
-  }
+if (!user) {
+  return res.status(404).json({ message: "User not found" });
+}
 
-  const passwordMatch = await bcrypt.compare(password, user.password);
+console.log(user.password);
+console.log(password);
 
-  if (!passwordMatch) {
-    return res.status(401).json({ message: 'Invalid password' });
-  }
+const passwordMatch = await bcrypt.compare(password, user.password);
 
-  const token = jwt.sign({ clientId: user._id }, process.env.MY_SECRET, { expiresIn: '5m' });
+if (!passwordMatch) {
+  return res.status(401).json({ message: "Invalid password" });
+}
 
-  res.cookie('token', token, {
-    maxAge: 15 * 24 * 60 * 60 * 1000,
-    httpOnly: true,
-    secure: true
-  });
+const token = jwt.sign({ clientId: user._id }, process.env.MY_SECRET, {
+  expiresIn: "5m",
+});
 
-  res.json({ token });
+res.cookie("token", token, {
+  maxAge: 15 * 24 * 60 * 60 * 1000,
+  httpOnly: true,
+  secure: true,
+});
+
+res.json({ token });
 }
 module.exports = { login, test };
--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,12 @@ const ownerrouter=require('./route/ownerRoute')
 
 const app = express();
 require('dotenv').config();
+const secret=process.env.MY_SECRET
+const port =process.env.PORT||5000
 
-const port =process.env.PORT||3000
 const MONGO_URL = process.env.MONGO_URL
+
+
 app.use(express.json());
 const connectionDataBase = require('./db/connectDb');
 
