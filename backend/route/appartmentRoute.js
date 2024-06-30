41fd6a6056ea1a8ac80dfeb27840b63c93d303ca
Ahmad <ahmadzer607@gmail.com>
2024-06-30 14:02:54 +0300

    f

--- backend/route/appartmentRoute.js
+++ backend/route/appartmentRoute.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '/images'));
+    cb(null, path.join(__dirname, './images'));
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
