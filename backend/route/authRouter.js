6fad1d18bd2f8397c27c132686ae8eca3b4de602
Ahmad <ahmadzer607@gmail.com>
2024-06-30 12:25:13 +0300

    ff

--- backend/route/authRouter.js
+++ backend/route/authRouter.js
@@ -4,7 +4,7 @@ const loginController=require('../controller/loginController')
 const regesterController = require('../controller/regesterControler');
 const cheakusertoken=require('../middlewre/cookieJwtAuth')
 
-router.post('/login', loginController.login)
+router.post('/login',loginController.login)
 router.get('/testlogin', loginController.test)
 router.post('/register',regesterController.register)
 module.exports=router
