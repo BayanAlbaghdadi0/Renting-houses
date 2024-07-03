bb68d861b582c3bb7e7c713348dbaf9d75ee0a45
shadowkok <khaledkokash963@gmail.com>
2024-07-03 21:51:44 +0300

    dd

--- frontend/renting-houses/src/components/OwnerHouses.jsx
+++ frontend/renting-houses/src/components/OwnerHouses.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
+
 const fetchOwnerHouses = async () => {
   try {
     const response = await fetch(`http://localhost:5000/owner/owners`, {
