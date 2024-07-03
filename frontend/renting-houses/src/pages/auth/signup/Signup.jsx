763a931b27f4e84f6daa3a01646e57550465f33f
shadowkok <khaledkokash963@gmail.com>
2024-07-03 15:17:12 +0300

    dsad

--- frontend/renting-houses/src/pages/auth/signup/Signup.jsx
+++ frontend/renting-houses/src/pages/auth/signup/Signup.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import houseImage from "../../../assets/matt-mutlu-unsplash.jpg";
-import { useSignup } from "../../../hooks/useSignup.jsx";
+import { useSignup } from "../../../../src/hooks/useSignUp.jsx";
 
 export const Signup = () => {
   const [inputs, setInputs] = useState({
